9d2b28985317ba6ff2ae506df99789115a6e86f2
MatiasMoralesEffectus <mmorales@effectussoftware.com>
2020-01-24 11:39:18 -0300

    Implement searching for useers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,32 @@
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :exercises     
-
+         
+  has_many :exercises
+  
   validates :first_name, presence: true
   validates :last_name, presence: true
   
   self.per_page = 10
-
-  def full_name 
+  
+  def full_name
     [first_name, last_name].join(" ")
+  end
+  
+  def self.search_by_name(name)
+    names_array = name.split(' ')
+
+    if names_array.size == 1
+      where('first_name LIKE ? or last_name LIKE ?',
+        "%#{names_array[0]}%", "%#{names_array[0]}%").order(:first_name)
+    else
+      where('first_name LIKE ? or first_name LIKE ? or last_name LIKE ?
+        or last_name LIKE ?', "%#{names_array[0]}%",
+        "%#{names_array[1]}%", "%#{names_array[0]}%",
+        "%#{names_array[1]}%").order(:first_name)
+    end
   end
 end
